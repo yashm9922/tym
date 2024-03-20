e38cf0796edc5526be7849ae0d474505f46ac261
yashwantmadane1412 <yashwantrajendra284@gmail.com>
2024-03-20 22:46:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Navbar1 } from './Components/Navbar1'
+
+function App() {
+
+  return (
+    <>
+      <Navbar1/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+ <div>asdaj</div>
+  )
+}
+
+export default Navbar
